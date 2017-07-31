1dbc8279a6c1252c264588f85dd825a89ff2a352
ChangfengHu <faker_322@outlook.com>
2017-07-31 20:26:28 +0800

    a1

--- bos-domain/src/main/java/cn/itcast/activemq/producer/queue/bos/domain/bc/DecidedZone.java
+++ bos-domain/src/main/java/cn/itcast/activemq/producer/queue/bos/domain/bc/DecidedZone.java
@@ -17,7 +17,7 @@ import java.util.Set;
 )
 public class DecidedZone  implements java.io.Serializable {
 
-        //ce11111111
+        //local master
      private String id;
      private Staff staff;
      private String name;
